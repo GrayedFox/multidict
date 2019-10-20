9cb1909a3720f96480ee9230a076a0bc7efab966
Che Fisher <che.fisher@gmail.com>
2019-10-20 13:15:10 +0200

    Fix empty textarea throwing exception when trying to set selected word
    Change display mode to inline-flex to prevent whitespace being added
    Fix suggestions appearing above instead of directly overlayed of word
    
    Apply github domain specific styles
    Remove unneeded styling from highlights.css
    
    Finish WordCarousel class and refactoring higlight.js carousel logic
    
    Update Spelling class to generate list of Words (not just strings)
    Add higlight.js helper to set currentMarkIndex
    Refactor helpers and move them from highlights.js into helpers file
    
    Refactor and update Higlighter to use ES6 class syntax
    Optimise content script to only destroy and re-create higlighter if changing editable nodes
    
    Force spelling recheck after adding/removing a word
    
    Fixes switching tabs breaking spell checking
    Fixes new tabs not having spell checking
    Refactors message sending between background and content script to be more efficient
    Update current mark during handle select method of highlight
    
    Reset port to current port after popup closes

--- src/content.js
+++ src/content.js
@@ -1,74 +1,117 @@
-const { debounce, getSelectedWord } = require('./helpers.js')
-const { highlight } = require('./deps/highlight/highlight.js')
+const { debounce, getSelectedWordBoundaries } = require('./helpers.js')
+const { Highlighter } = require('./deps/highlight/highlight.js')
 
-const messageHandler = browser.runtime.connect({ name: 'content' })
 const editableFields = ['TEXTAREA']
 
-let node
+let contentPort = null
+let highlighter = null
+let editableNode = null
 let oldValue = ''
 
-// listens to all keyup events
+// function is debounced on all keyup and click events
 async function edit (event) {
-  node = event.target
-
+  const target = event.target
   // don't spellcheck non-editable or unsupported fields
-  if (!editableFields.includes(node.nodeName) || node.contentEditable === false) {
+  if (!editableFields.includes(target.nodeName) || target.contentEditable === false) {
     return
   }
 
   // don't spellcheck if text is identical to last spellchecked text
-  if (node.getAttribute('data-multidict-generated') && (node.value === oldValue)) {
+  if (target.getAttribute('data-multidict-generated') && (target.value === oldValue)) {
     return
   }
 
-  oldValue = node.value
+  // tell background script to connect to active tab if content port undefined
+  if (!contentPort) {
+    await browser.runtime.sendMessage({ type: 'connectToActiveTab' })
+  }
 
-  const detectedLanguage = await browser.i18n.detectLanguage(node.value)
+  editableNode = event.target
+  oldValue = editableNode.value
+
+  const detectedLanguage = await browser.i18n.detectLanguage(editableNode.value)
   const language = detectedLanguage.isReliable
     ? detectedLanguage.languages[0].language
     : 'unreliable'
 
-  messageHandler.postMessage({
-    type: 'check',
+  contentPort.postMessage({
+    type: 'spellCheck',
     detectedLanguage: language,
-    content: node.value
+    content: editableNode.value
   })
 }
 
-// listens to all incoming messages from the background script
-messageHandler.onMessage.addListener((message) => {
-  switch (message.type) {
-    case 'greeting':
-      console.log(message.greeting)
-      break
+// properly resets all values and cleans up leftover html elements on disconnect event
+function disconnect (p) {
+  if (p.error) {
+    console.warn(`MultiDict: disconnected due to an error: ${p.error.message}. Please try refreshing the page`)
+  }
+  if (highlighter) {
+    highlighter.destroy()
+  }
+  contentPort.onMessage.removeListener(messageHandler)
+  contentPort.onDisconnect.removeListener(disconnect)
+  contentPort = null
+  highlighter = null
+  oldValue = ''
+}
 
+// handles incoming connections from the background script (port should update with each tab change)
+async function connectionHandler (port, info) {
+  if (!contentPort) {
+    contentPort = port
+    contentPort.onMessage.addListener(messageHandler)
+    contentPort.onDisconnect.addListener(disconnect)
+  }
+}
+
+// handles all incoming messages from the background script
+function messageHandler (message) {
+  switch (message.type) {
     case 'highlight':
-      highlight(node, {
-        misspeltWords: message.spelling.misspeltWords,
-        suggestions: message.spelling.suggestions,
-        className: 'red'
-      })
+      if (highlighter && !editableNode.getAttribute('data-multidict-generated')) {
+        highlighter.destroy()
+        highlighter = null
+      }
+
+      if (!highlighter) {
+        highlighter = new Highlighter(editableNode, message.spelling, 'red')
+      } else {
+        highlighter.setSpelling(message.spelling)
+      }
       break
 
     case 'add':
     case 'remove':
-      messageHandler.postMessage({
+      contentPort.postMessage({
         type: message.type,
-        word: getSelectedWord().text
+        word: getSelectedWordBoundaries()[0]
       })
+      // clearing oldValue after add/remove ensures we recheck spelling despite identical content
+      oldValue = ''
       break
 
     default:
-      console.warn(`MultiDict: unrecognized background message ${message}`)
+      console.warn(`MultiDict: unrecognized background message ${JSON.stringify(message)}`)
   }
-})
+}
 
-// for debugging purposes, log disconnect error
-messageHandler.onDisconnect.addListener((p) => {
-  if (p.error) {
-    console.warn(`MultiDict: disconnected due to an error: ${p.error.message}. Please try refreshing the page`)
-  }
-})
+// App Control/Execution Flow
+// 1. Page listeners generate Spellings of currently focused/current editable node content
+// 2. Single Highlighter instance (requires Spelling) attached to focused/current editable node
+// 3. Highlighter instance has own listeners to keep it in sync with editable node and allow user
+//    interaction
+// 4. Highlighter instance generates WordCarousel (suggestions node) as needed and controls the
+//    carousel (WordCarousel does not have own listeners)
+//
+// Architectural/Design Decisions
+// - Helper functions are Class agnostic (helpers can work with a class instance but never
+//   instantiate or import/require classes.js)
+// - Classes can and do use Helper fuctions/methods during instantiation
+// - All clean text for usage in app should be cleaned using core cleanText helper function (with
+//   or without specific params)
+
+browser.runtime.onConnect.addListener(connectionHandler)
 
 document.body.addEventListener('keyup', debounce(edit, 700))
 document.body.addEventListener('click', debounce(edit, 700))
