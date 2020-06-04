a3fe1e5bfb1444514f25080805d288142cf80461
Che Fisher <che.fisher@gmail.com>
2020-06-04 17:50:05 +0200

    Fix remove word using popup not forcing a recheck
    Update npm run serve to also build app before serving
    
    Update npm and packages
    
    Update readme
    
    Add boolean setting to popup
    Add settings retrieval when extension loads
    Add update persistent settings via popup window
    Remove debug logging
    Update package, manifest, and web-ext files to workaround weird bug
    
    Use settings object instead of array after retrieving and storing values
    
    Add feature to remove native spell checking from all textareas

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,54 +1,110 @@
 const messageHandler = browser.runtime.connect({ name: 'popup' })
 
-const personalDictionary = document.querySelector('#dictionary')
-const wordsList = document.querySelector('.words')
-const arrowDiv = document.querySelector('.arrow')
-const wordItem = document.createElement('li')
+const dictionary = document.querySelector('#dictionary')
+const settings = document.querySelector('#settings')
+const listItem = document.createElement('li')
 const hrefChild = document.createElement('a')
+const input = document.createElement('input')
+const label = document.createElement('label')
 
-wordItem.classList.add('ownWord')
-hrefChild.setAttribute('href', '#')
-wordItem.appendChild(hrefChild)
+const optionLabels = {
+  disableNativeSpellcheck: 'Disable duplicate spellchecking (requires page refresh)'
+}
 
 let showingWords = false
+let showingOptions = false
 
-function removeWord (node) {
-  messageHandler.postMessage({ type: 'remove', word: node.textContent })
-  wordsList.removeChild(node)
-}
+listItem.appendChild(hrefChild)
+hrefChild.setAttribute('href', '#')
+input.type = 'checkbox'
 
-function hideWords () {
-  while (wordsList.firstChild) {
-    wordsList.removeChild(wordsList.firstChild)
+function hideListItems (list, words) {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild)
   }
-  showingWords = false
+
+  if (words) {
+    showingWords = false
+  } else {
+    showingOptions = false
+  }
 }
 
-function showWords (customWords) {
+function populateList (list, customWords) {
   customWords.forEach((word) => {
-    wordItem.textContent = word
-    wordsList.appendChild(wordItem.cloneNode(true))
+    listItem.textContent = word
+    list.appendChild(listItem.cloneNode(true))
   })
+  listItem.textContent = ''
   showingWords = true
 }
 
+function populateOptions (list, options) {
+  // options are an array of strings stored like so: ['key-value, key-value, key-value']
+  for (const [key, value] of Object.entries(options)) {
+    input.id = key
+    input.name = key
+    input.value = value
+    input.checked = value
+    label.textContent = optionLabels[key]
+    label.setAttribute('for', key)
+    label.appendChild(input)
+    listItem.appendChild(label)
+    list.appendChild(listItem.cloneNode(true))
+  }
+  showingOptions = true
+}
+
+function getSettingsFromPopup () {
+  const settingsArray = []
+  document.querySelectorAll('.options input').forEach((setting) => {
+    settingsArray.push(`${setting.id}-${setting.value}`)
+  })
+  return settingsArray
+}
+
+// word related click event listener (fetching, hiding, and removing custom words)
 function handleWords (e) {
-  if (showingWords && e.target.parentNode === personalDictionary) {
-    arrowDiv.textContent = '▶'
-    hideWords()
+  const wordsList = document.querySelector('.words')
+  const dictionaryArrow = document.querySelector('#dictionary .arrow')
+  if (showingWords && e.target.parentNode === dictionary) {
+    dictionaryArrow.textContent = '▶'
+    hideListItems(wordsList, 'words')
   } else if (!showingWords) {
-    arrowDiv.textContent = '▼'
+    dictionaryArrow.textContent = '▼'
     messageHandler.postMessage({ type: 'getCustomWords' })
-  } else if (e.target.classList.contains('ownWord')) {
-    removeWord(e.target)
+  } else if (e.target.matches('.words li')) {
+    messageHandler.postMessage({ type: 'remove', word: e.target.textContent })
+    wordsList.removeChild(e.target)
+  }
+}
+
+// checkbox related input event listener (displaying and updating persistent options)
+function handleOptions (e) {
+  const optionsList = document.querySelector('.options')
+  const optionsArrow = document.querySelector('#settings .arrow')
+  if (showingOptions && e.target.parentNode === settings) {
+    optionsArrow.textContent = '▶'
+    hideListItems(optionsList)
+  } else if (!showingOptions) {
+    optionsArrow.textContent = '▼'
+    messageHandler.postMessage({ type: 'getCustomSettings' })
+  } else if (e.target.matches('.options input')) {
+    e.target.value = e.originalTarget.checked
+    messageHandler.postMessage({ type: 'saveSettings', settings: getSettingsFromPopup() })
   }
 }
 
 // listen to incoming messages from background script
 messageHandler.onMessage.addListener((message) => {
   if (message.type === 'getCustomWords') {
-    showWords(message.customWords)
+    populateList(document.querySelector('.words'), message.customWords)
+  }
+
+  if (message.type === 'getCustomSettings') {
+    populateOptions(document.querySelector('.options'), message.customSettings)
   }
 })
 
-personalDictionary.addEventListener('click', handleWords)
+dictionary.addEventListener('click', handleWords)
+settings.addEventListener('click', handleOptions)
